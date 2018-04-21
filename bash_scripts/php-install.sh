41f561dab42731438dd39ece07d831b198fd801f
Ubuntu <ubuntu@ip-172-31-45-50.us-east-2.compute.internal>
2018-04-21 02:43:39 +0000

    Update php script to install successfully
    needed to update apt-get before using php 7.0 cli install.
    this will now work the first time.

--- bash_scripts/php-install.sh
+++ bash_scripts/php-install.sh
@@ -4,8 +4,11 @@ sudo mkdir /var/data
 sudo cp -R ../php-file-demo/data/ /var/
 sudo chmod 775 -R /var/data/
 
-sudo apt install php7.0-cli
+sudo apt-get update
+sudo apt-get install php7.0-cli -y
 
-nohup ../php-file-demo/source/read_files1.php &
-nohup ../php-file-demo/source/read_files2.php &
-nohup ../php-file-demo/source/read_files3.php &
+nohup php ../php-file-demo/source/read_files1.php &
+nohup php ../php-file-demo/source/read_files2.php &
+nohup php ../php-file-demo/source/read_files3.php &
+
+ps aux | grep php
